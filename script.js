ea3d36fc45facb197fa9555dd0ff27a6edd3197f
Jagrit Krishna <87370572+Jagrit0007@users.noreply.github.com>
2023-12-16 14:35:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function functionName() {
+  // Get the name of the current function using arguments.callee.name
+  const currentFunctionName = arguments.callee.name;
+
+  // Display the name using an alert statement
+  alert( currentFunctionName);
+}
